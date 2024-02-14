6986d684c6555b5ee3714ab052e7f8aa086a9b88
theniyal2427 <159756215+theniyal2427@users.noreply.github.com>
2024-02-14 10:26:24 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.lazy').owlCarousel({
+    items:1,
+    autoplay:true,
+    autoplayTimeout:8000,
+    lazyLoad:true,
+    loop:true,
+    margin:10
+});
+
+
+$('.protfolio').owlCarousel({
+    items:1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    lazyLoad:true,
+    loop:true,
+    margin:10
+});
